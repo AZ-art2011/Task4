3e9c39939e8a979f73fc0289e2f8b377e87b5872
Alexander <mypost@example.com>
2022-12-17 15:46:34 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4
+
+Console.WriteLine("Вас приветствует программа сравнения трех чисел");
+
+Console.WriteLine("Введите первое число");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numThree = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (numFirst > numSecond){
+    max = numFirst;
+}
+else
+{
+    max = numSecond;
+}
+
+if (numThree > max){
+    max = numThree;
+}
+
+Console.WriteLine($"Наиюольшее число {max}");
